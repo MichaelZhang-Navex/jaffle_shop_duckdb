c1c9db5ea553a0bccce9cff5598640c1e90e44a1
Michael Zhang <michael.zhang@navex.com>
2025-01-16 18:41:02 +0000

    disable ST07

--- models/marts/payments_by_type.sql
+++ models/marts/payments_by_type.sql
@@ -5,13 +5,14 @@ with order_payments as (
         p.amount
     from {{ ref('stg_orders') }} as o
     left join {{ ref('stg_payments') }} as p
-        on o.order_id = p.order_id
+        using (order_id)
+        -- on o.order_id = p.order_id
     where p.payment_method in ('coupon', 'gift_card')
 )
 
 select
     order_date,
-    SUM(amount) as amount
+    sum(amount) as amount
 from order_payments
 group by 1
 order by 1
